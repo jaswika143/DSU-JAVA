620d39529cffa8f03946df992f8ae4c2d3bc1301
jaswika143 <jaswikamalagaveli@gmail.com>
2025-09-04 12:12:06 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class length{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string");
+        String str= sc.nextLine();
+        System.out.println("Length of the string:" + str.length());
+    }
+    
+}
