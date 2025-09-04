a7c965015003a7c30addd57f7d7cb0e51e5bb8bd
jaswika143 <jaswikamalagaveli@gmail.com>
2025-09-04 12:04:14 +0530

    Add files via upload

---
+++ arraysquare.java
@@ -0,0 +1,11 @@
+public class arraysquare {
+    public static void main(String [] args){
+        int[] squares = new int[10];
+        for(int i=0;i<numbers.length;i++){
+            Square[i]=(i+1)*(i+1);
+        
+        }
+        System.out.println("squares of numbers 1 to 10");
+        for 
+    }
+}
