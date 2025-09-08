1b806d2cc2f325b90ba2c7f9ae1e3b8849447e0b
jaswika143 <jaswikamalagaveli@gmail.com>
2025-09-08 21:19:28 +0530

    Add files via upload

---
+++ increment.java
@@ -0,0 +1,15 @@
+public class increment{
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println("Orginal:" +x);
+        System.out.println("post increment:"+(x++));
+        System.out.println("After post increment:"+x);
+        System.out.println("Pre-increment:"+(++x));
+        System.out.println("After pre-increment:"+x);
+        System.err.println("Post-decrement:"+(x--));
+        System.out.println("Pre- decrement:"+(--x));
+        System.out.println("After pre-increment:"+x);      
+        
+    }
+    
+}
